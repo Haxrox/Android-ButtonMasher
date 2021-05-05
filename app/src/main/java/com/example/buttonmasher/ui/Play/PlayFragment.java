f4517e336347cbec8229e41c1dfe45c1751b9126
Haxrox <56497846+Haxrox@users.noreply.github.com>
2021-05-04 19:55:04 -0700

    removed Game class and used PlayViewModel isntead

--- app/src/main/java/com/example/buttonmasher/ui/Play/PlayFragment.java
+++ app/src/main/java/com/example/buttonmasher/ui/Play/PlayFragment.java
@@ -1,8 +1,7 @@
 package com.example.buttonmasher.ui.Play;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,40 +11,131 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.preference.PreferenceManager;
 
-import com.example.buttonmasher.Game;
 import com.example.buttonmasher.MainActivity;
 import com.example.buttonmasher.R;
 
 public class PlayFragment extends Fragment {
 
     private PlayViewModel playViewModel;
+    // Constants
+    private static final int GAME_LENGTH = 15;
+    private static final int MIN_CPU_SPEED = 75;
+    private static final int MAX_CPU_SPEED = 200;
+
+    // Scores
+    int cpuClicks, userClicks;
+
+    // UI Objects
+    private TextView timeLeft, userBox, cpuBox;
+    private Button startButton, clickButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        playViewModel =
-                new ViewModelProvider(this).get(PlayViewModel.class);
+        playViewModel = new ViewModelProvider(this).get(PlayViewModel.class);
+        // Initializing variables to be same as the data in the PlayViewModel
+        cpuClicks = playViewModel.getCpuClicks();
+        userClicks = playViewModel.getUserClicks();
+
+        // UI Objects
         View root = inflater.inflate(R.layout.fragment_play, container, false);
+        timeLeft = root.findViewById(R.id.timeLeft);
+        userBox = root.findViewById(R.id.userScoreBox);
+        cpuBox = root.findViewById(R.id.cpuScoreBox);
+        startButton = root.findViewById(R.id.buttonStart);
+        clickButton = root.findViewById(R.id.clickButton);
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-
-        final Button startButton = root.findViewById(R.id.buttonStart);
+        // Button listeners
+        // Start the game
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Game(root, MainActivity.getProfile(), sharedPreferences.getInt("GameLength", 15));
+                newGame();
             }
         });
+        // Increment user score by 1
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userClicks++;
+                userBox.setText(String.valueOf(userClicks));
+            }
+        });
+
         return root;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    public void newGame() {
+        // Initializing UI
+        timeLeft.setText(String.valueOf(GAME_LENGTH));
+        userBox.setText(String.valueOf(playViewModel.getUserClicks()));
+        cpuBox.setText(String.valueOf(playViewModel.getCpuClicks()));
 
+        // Variables
+        userClicks = 0;
+        cpuClicks = 0;
+        playViewModel.setUserClicks(userClicks);
+        playViewModel.setCpuClicks(cpuClicks);
+
+        // CPU
+        long computerInterval = (long) ((Math.random()*(MAX_CPU_SPEED - MIN_CPU_SPEED) + MIN_CPU_SPEED));
+        CountDownTimer cpuTimer = new CountDownTimer((long)GAME_LENGTH * 1000, computerInterval) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                cpuClicks++;
+                cpuBox.setText(String.valueOf(cpuClicks));
+            }
+            @Override
+            public void onFinish() {
+                CountDownTimer cpuTimer = playViewModel.getCpuTimer();
+                cpuTimer.cancel();
+            }
+        };
+        playViewModel.setCpuTimer(cpuTimer);
+
+        // GameTimer
+        CountDownTimer gameTimer = new CountDownTimer((long)GAME_LENGTH * 1000, (long)1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeft.setText(String.format("%ds", millisUntilFinished / 1000));
+            }
+            @Override
+            public void onFinish() {
+                CountDownTimer cpuTimer = playViewModel.getCpuTimer();
+                cpuTimer.cancel();
+                endGame();
+            }
+        };
+        playViewModel.setGameTimer(gameTimer);
+
+        // Beginning
+        gameTimer.start();
+        cpuTimer.start();
+        clickButton.setEnabled(true);
+        startButton.setEnabled(false);
+        MainActivity.getInstance().displayToast("The computer is clicking at " + computerInterval + "ms, good luck!");
+    }
+
+    public void endGame() {
+        // Setting to begin state
+        clickButton.setEnabled(false);
+        startButton.setEnabled(true);
+        // Getting winner
+        int clickDifference = userClicks - cpuClicks;
+        if (userClicks > cpuClicks) { // User won
+            // profile.incrementWins();
+            MainActivity.getInstance().displayToast("You out-clicked the computer by " + Math.abs(clickDifference) + " clicks!");
+        } else if (cpuClicks > userClicks) { // CPU won
+            MainActivity.getInstance().displayToast("The computer has out-clicked you by " + Math.abs(clickDifference) + " clicks!");
+        } else { // No-one won
+            MainActivity.getInstance().displayToast("You and the computer has tied!");
+        }
+        // profile.incrementClicks(userClicks + clickDifference);
     }
 }
--- app/src/main/java/com/example/buttonmasher/ui/Play/PlayViewModel.java
+++ app/src/main/java/com/example/buttonmasher/ui/Play/PlayViewModel.java
@@ -1,16 +1,55 @@
 package com.example.buttonmasher.ui.Play;
 
-import androidx.lifecycle.LiveData;
+import android.os.CountDownTimer;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.buttonmasher.Game;
-
 public class PlayViewModel extends ViewModel {
 
-    private MutableLiveData<Game> game;
+    private MutableLiveData<Integer> cpuClicks;
+    private MutableLiveData<Integer> userClicks;
+    private MutableLiveData<CountDownTimer> gameTimer;
+    private MutableLiveData<CountDownTimer> cpuTimer;
 
     public PlayViewModel() {
+        cpuClicks = new MutableLiveData<>();
+        cpuClicks.setValue(0);
+        userClicks = new MutableLiveData<>();
+        userClicks.setValue(0);
+        gameTimer = new MutableLiveData<>();
+        cpuTimer = new MutableLiveData<>();
+    }
 
+    public int getCpuClicks() {
+        return cpuClicks.getValue();
+    }
+
+    public void setCpuClicks(int cpuClicks) {
+        this.cpuClicks.setValue(cpuClicks);
+    }
+
+    public int getUserClicks() {
+        return userClicks.getValue();
+    }
+
+    public void setUserClicks(int userClicks) {
+        this.userClicks.setValue(userClicks);
+    }
+
+    public CountDownTimer getGameTimer() {
+        return gameTimer.getValue();
+    }
+
+    public void setGameTimer(CountDownTimer gameTimer) {
+        this.gameTimer.setValue(gameTimer);
+    }
+
+    public CountDownTimer getCpuTimer() {
+        return cpuTimer.getValue();
+    }
+
+    public void setCpuTimer(CountDownTimer cpuTimer) {
+        this.cpuTimer.setValue(cpuTimer);
     }
 }
